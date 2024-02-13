8b84eca81aeb1ba8d9d6c64094dcc3023bdd57fa
sasha-poirier-pix4d <sasha.poirier@pix4d.com>
2024-02-13 16:24:17 +0100

    AR-4917 init xcode project

---
+++ TrimblePlayground/TrimblePlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrimblePlaygroundApp.swift
+//  TrimblePlayground
+//
+//  Created by Sasha Poirier on 01.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct TrimblePlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
